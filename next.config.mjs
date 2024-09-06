13a43f9f6ba28bdbee33694dab289381b80d87c1
Danylo Mezentsev <dmezents@cisco.com>
2024-09-06 14:19:33 +0200

    dmeze: Add gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: false,
+    },
+    basePath: '/second-diamond',
+    assetPrefix: '/second-diamond/',
+};
 
 export default nextConfig;
