8967c5a26a005620d293d79ad99e3ad40904b724
Danylo Mezentsev <dmezents@cisco.com>
2024-09-25 23:39:17 +0200

    dmeze: Add speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line camelcase
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   )
