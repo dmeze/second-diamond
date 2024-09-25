254873d662328d5acb1829375c0ee7add7264069
Danylo Mezentsev <dmezents@cisco.com>
2024-09-25 15:12:33 +0200

    dmeze: Always autoplay video

--- src/app/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/app/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,37 +1,49 @@
 'use client'
 
 import Image from 'next/image'
+import { useEffect, useRef } from 'react'
 
-const BackgroundVideo = ({ children }) => (
-  <div className="relative h-screen w-full">
-    <div className="absolute left-0 top-0 -z-10 size-full">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="pointer-events-none absolute left-0 top-0 size-full object-cover"
-      >
-        <source src="/background.webm" type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="flex size-full items-center justify-center gap-16 md:px-12">
-      <div className="relative flex justify-center  ">
-        <div className="absolute -right-1/2 top-[-175%] flex size-[800px] items-center justify-center md:right-auto">
-          <Image
-            src="/circle.png"
-            alt="Circular Background"
-            fill
-            priority
-            sizes="(max-width: 768px) 100vw"
-            className="object-cover opacity-40 md:opacity-60"
-          />
+const BackgroundVideo = ({ children }) => {
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    if (videoRef.current && videoRef.current.paused) {
+      videoRef.current.play()
+    }
+  }, [])
+
+  return (
+    <div className="relative h-screen w-full">
+      <div className="absolute left-0 top-0 -z-10 size-full">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          ref={videoRef}
+          className="pointer-events-none absolute left-0 top-0 size-full object-cover"
+        >
+          <source src="/background.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="flex size-full items-center justify-center gap-16 md:px-12">
+        <div className="relative flex justify-center  ">
+          <div className="absolute -right-1/2 top-[-175%] flex size-[800px] items-center justify-center md:right-auto">
+            <Image
+              src="/circle.png"
+              alt="Circular Background"
+              fill
+              priority
+              sizes="(max-width: 768px) 100vw"
+              className="object-cover opacity-40 md:opacity-60"
+            />
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default BackgroundVideo
