272033479323120838ab075219a0e24f975c0cf4
Danylo Mezentsev <dmezents@cisco.com>
2024-09-25 15:22:26 +0200

    dmeze: Show background image in the low power mode

--- src/app/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/app/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,31 +1,46 @@
 'use client'
 
 import Image from 'next/image'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const BackgroundVideo = ({ children }) => {
   const videoRef = useRef(null)
+  const [isLowPowerMode, setIsLowPowerMode] = useState(false)
 
   useEffect(() => {
-    if (videoRef.current && videoRef.current.paused) {
-      videoRef.current.play()
+    if (videoRef.current) {
+      const promise = videoRef.current.play()
+      promise.catch(() => {
+        setIsLowPowerMode(true)
+      })
     }
   }, [])
 
   return (
     <div className="relative h-screen w-full">
       <div className="absolute left-0 top-0 -z-10 size-full">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          ref={videoRef}
-          className="pointer-events-none absolute left-0 top-0 size-full object-cover"
-        >
-          <source src="/background.webm" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
+        {!isLowPowerMode ? (
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            ref={videoRef}
+            className="pointer-events-none absolute left-0 top-0 size-full object-cover"
+          >
+            <source src="/background.webm" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <Image
+            src="/backgroundImage.png"
+            alt="Background Image"
+            fill
+            priority
+            sizes="(max-width: 768px) 100vw"
+            className="object-cover"
+          />
+        )}
       </div>
       <div className="flex size-full items-center justify-center gap-16 md:px-12">
         <div className="relative flex justify-center  ">
